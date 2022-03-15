c6e0976f17198bb7bdad691dd94ca73d579bb1e1
Matt4292 <matt4292002@gmail.com>
2022-03-15 00:51:21 -0500

    Final product

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Day Planner
+
+## Description 
+In this project I was given HTML and CSS starter code, my task was to write JavaScript to make it function according to the given criteria. Users can enter text in the blank input fields and when they click a save button on the right side it will save the data they entered to their local storage and have it load back into that field when they reload the page. I used Moment.js to show the current date in the header and to change the background color of the input fields according to the time. If the hour associated with the field has passed it will have grey background, the current hour will show up red, and any future hours are green. 
+
+## Screenshot
+![Screenshot of the final product](day-planner.jpg)
+
+## Link
+https://matt4292.github.io/day-planner/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-
 var container = $(".container");
-
-
 var hours = [
   {
     time:"9AM",
-    hourNumber:09
+    hourNumber:09,
   },
   {
     time:"10AM",
@@ -29,7 +26,7 @@ var hours = [
   },
   {
     time:"3PM",
-    hourNumber:20
+    hourNumber:15
   },
   {
     time:"4PM",
@@ -38,15 +35,11 @@ var hours = [
   {
     time:"5PM",
     hourNumber:17
-  },
-  ];
-
-
-
+  }
+];
 
 // Displays current day and date in the header
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
-
 
 var currentHour = 0
 
@@ -60,20 +53,24 @@ hours.forEach(function(hour){
   var hourBlock = $("<div>");
   hourBlock.addClass("row");
   container.append(hourBlock);
+  
 
   //lists the hours on the left side
-  var listOfHours = $("<div>");
+  var listOfHours = $("<label>");
   listOfHours.addClass("hour col-2");
   hourBlock.append(listOfHours);
   listOfHours.text(hours[hour].time);
 
   //makes the field to enter info
-  var textField = $("<input type='text'>");
+  var textField = $("<input type='text' value='test'>");
   textField.addClass("description col-8");
   hourBlock.append(textField);
 
+  // loads any data in local storage as default text
+  textField.attr("id", hrNumber);
+  textField.attr("value", localStorage.getItem(hour))
+
   //styles the field depending on time of day
-  console.log(moment().hour())
   if (hrNumber<moment().hour()){
     textField.addClass("past");
   } else if (hrNumber>moment().hour()){
@@ -85,12 +82,30 @@ hours.forEach(function(hour){
   //makes save button
   var saveBtn = $("<button>");
   saveBtn.addClass("saveBtn col-2");
+  saveBtn.addClass("far fa-save");
   hourBlock.append(saveBtn);
-
-
 });
 
 
-
-
-
+// saves input field text to local storage
+$(".saveBtn").on("click", function(event) {
+  event.preventDefault();
+  var input9 = document.getElementById("9");
+  var input10 = document.getElementById("10");
+  var input11 = document.getElementById("11");
+  var input12 = document.getElementById("12");
+  var input1 = document.getElementById("13");
+  var input2 = document.getElementById("14");
+  var input3 = document.getElementById("15");
+  var input4 = document.getElementById("16");
+  var input5 = document.getElementById("17");
+  localStorage.setItem("0", input9.value)
+  localStorage.setItem("1", input10.value)
+  localStorage.setItem("2", input11.value)
+  localStorage.setItem("3", input12.value)
+  localStorage.setItem("4", input1.value)
+  localStorage.setItem("5", input2.value)
+  localStorage.setItem("6", input3.value)
+  localStorage.setItem("7", input4.value)
+  localStorage.setItem("8", input5.value)
+});
